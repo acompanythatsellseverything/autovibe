e244354c36fe22c56586a32aa8a15790ac355ac8
wildmll <hanych.maksym@student.uzhnu.edu.ua>
2026-01-09 18:56:39 +0200

    Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <meta httpEquiv="Content-Security-Policy" content="upgrade-insecure-requests" />
+      </head>
       <body
         className={`${inter.variable} antialiased`}
         suppressHydrationWarning
